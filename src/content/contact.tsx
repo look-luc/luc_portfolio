60db20943f44a8ba9235952e36850a3bba6b5bc7
Luc De Nardi <denardiluc@gmail.com>
2025-06-16 09:48:38 -0400

    working on gh pages plus added linkedin

--- src/content/contact.tsx
+++ src/content/contact.tsx
@@ -13,7 +13,15 @@ function Contact() {
           <p className="address">luc [dot] denardi [at] colorado [dot] edu</p>
         </div>
       </div>
-      <div className="socials"></div>
+      <div className="social_cont">
+        <div className="socials">
+          Linkedin:
+          <a href="https://www.linkedin.com/in/luc-de-nardi-0682b4278/">
+            {" "}
+            link to my Linkedin
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
