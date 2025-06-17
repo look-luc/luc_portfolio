ae5d6e5ecccdf9b69dd21c800508b66040bf7b47
Luc De Nardi <denardiluc@gmail.com>
2025-06-17 06:55:22 -0400

    about-me in progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router";
 import "./App.css";
 import Navigation_Bar from "./navigation_bar/navigation_bar";
+import self_image from "./assets/self.png";
 
 function App() {
   const navigate = useNavigate();
@@ -9,6 +10,22 @@ function App() {
     <div className="App">
       <Navigation_Bar />
       <header className="App-header">Luc De Nardi</header>
+      <div className="about-me-container">
+        <div className="about-me">
+          <div className="title">About me:</div>
+          <p className="about-body">
+            I am an incoming Grad in the CLASIC program at the University of
+            Colorado Boulder.
+          </p>
+          <p className="about-body">
+            I got my BA in linguistics with minors in cognitive science and
+            computer science at the University of Delaware.
+          </p>
+        </div>
+        <div className="self-image">
+          <img src={self_image} alt="self" width={"300"} />
+        </div>
+      </div>
     </div>
   );
 }
