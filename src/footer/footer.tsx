a43a98afbb4b46ef9e0dd8db430d46a8cc9cfdce
Luc De Nardi <denardiluc@gmail.com>
2025-06-17 12:43:51 -0400

    fixed nav bar, add footer, finish interest, no papers

---
+++ src/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>&#169; Luc De Nardi. Made from Typescript React App.</p>
+    </div>
+  );
+}
+export default Footer;
