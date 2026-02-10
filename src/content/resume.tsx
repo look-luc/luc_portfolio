9881017905553a82225f58f1dbd67a4c99d220a0
lucdenardi <denardiluc@gmail.com>
2026-02-10 12:38:42 -0700

    reworked resume, just doc formatting

--- src/content/resume.tsx
+++ src/content/resume.tsx
@@ -1,30 +1,26 @@
-import 'react'
-import './resume.css'
+import "react";
+import "./resume.css";
 import Navigation_Bar from "../navigation_bar/navigation_bar";
-import { Button } from 'react-bootstrap';
-import resume from '../assets/resume.pdf'
+import { Button } from "react-bootstrap";
+import resume from "../assets/resume.pdf";
 
-function Resume(){
-    const handleDownload = () => {
-        const link = document.createElement("a");
-        link.href = resume;
-        link.download = "luc_resume.pdf";
-        link.click();
-    };
-    return(
-        <div className='Resume'>
-            <Navigation_Bar/>
-            <header className="resume-header">Resume</header>
-            <div className='download_wrapper'>
-                <Button
-                    className='download_button not'
-                    onClick={handleDownload}
-                >
-                    Click here to download resume
-                </Button>
-            </div>
-        </div>
-        
-    )
+function Resume() {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = resume;
+    link.download = "luc_resume.pdf";
+    link.click();
+  };
+  return (
+    <div className="Resume">
+      <Navigation_Bar />
+      <header className="resume-header">Resume</header>
+      <div className="download_wrapper">
+        <Button className="download_button not" onClick={handleDownload}>
+          Click here to download resume
+        </Button>
+      </div>
+    </div>
+  );
 }
-export default Resume
+export default Resume;
