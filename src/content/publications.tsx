02f6726ebbd9055e251cf27d0d9c04cafe5d4c24
lucdenardi <denardiluc@gmail.com>
2026-02-10 09:33:14 -0700

    formated button

--- src/content/publications.tsx
+++ src/content/publications.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./publications.css";
+import Navigation_Bar from "../navigation_bar/navigation_bar";
+import Footer from "../footer/footer";
 
 function Publications() {
-  return <div></div>;
+  return (
+    <div className="pub">
+      <Navigation_Bar />
+      <div className="wrapper"></div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Publications;
