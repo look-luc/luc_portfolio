153aebd7744f18410f7acb0836640927016f7fb4
lucdenardi <denardiluc@gmail.com>
2026-02-10 12:30:49 -0700

    added publication

--- src/content/publications.tsx
+++ src/content/publications.tsx
@@ -7,7 +7,27 @@ function Publications() {
   return (
     <div className="pub">
       <Navigation_Bar />
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <header className="pub-header">Publications</header>
+        <div className="main-wrapper">
+          <div className="pub-container">
+            <div className="publication">
+              <span className="pub-title">
+                Rethinking Polarity Detection: When BPE Fails Across Scripts
+              </span>
+              <h4>K H Manodnya, De Nardi Luc. (2026)</h4>
+              <p>
+                <ul>
+                  <li>Accepted at AbjadNLP and EACL 2026 conference</li>
+                  <li>Accepted at EACL 2026 workshop LoResLM</li>
+                  <li>Presented at EACL conference in Rabat Morocco</li>
+                  <li>Python ML packages</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
