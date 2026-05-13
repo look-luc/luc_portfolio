5142834df3cb87628caa9f95844330f40c5abaf8
look-luc <denardiluc@gmail.com>
2026-05-13 09:03:26 -0600

    updated publication

--- src/content/publications.tsx
+++ src/content/publications.tsx
@@ -13,14 +13,30 @@ function Publications() {
           <div className="pub-container">
             <div className="publication">
               <span className="pub-title">
-                Rethinking Polarity Detection: When BPE Fails Across Scripts
+                <a href="https://aclanthology.org/2026.abjadnlp-1.2/">
+                  Rethinking Polarity Detection: When BPE Fails Across Scripts
+                </a>
               </span>
               <h4>K H Manodnya, De Nardi Luc. (2026)</h4>
               <p>
                 <ul>
                   <li>
-                    Presented at EACL 2026 conference for AbjadNLP and LoResLM
-                    workshop
+                    Presented at EACL 2026 conference for AbjadNLP workshop
+                  </li>
+                </ul>
+              </p>
+              <span className="pub-title">
+                {" "}
+                <a href="https://aclanthology.org/2026.loreslm-1.4/">
+                  When Multilingual Evaluation Assumptions Fail: Tokenization
+                  Effects Across Scripts
+                </a>
+              </span>
+              <h4>K H Manodnya, De Nardi Luc. (2026)</h4>
+              <p>
+                <ul>
+                  <li>
+                    Presented at EACL 2026 conference for LoResLM workshop
                   </li>
                 </ul>
               </p>
